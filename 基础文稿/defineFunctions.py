db92b3646e4a3a9b09b5e1b3cddffc6df0210f69
frankie-mbp <877672473@qq.com>
2018-04-29 21:18:12 +0800

    commit class variable define

--- 基础文稿/defineFunctions.py
+++ 基础文稿/defineFunctions.py
@@ -1,2 +1,10 @@
-lists = [value * 2 for value in range(1,11)]
-print(list)
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
+    
+instance = Student('frankie', 99)
+instance.print_score()
